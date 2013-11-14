d815562280f4d38db42c3cd8258d9db0141a4767
odorsey <odorsey@live.unc.edu>
2013-11-14 13:42:20 -0500

    Update home.py

--- home.py
+++ home.py
@@ -1,23 +1,13 @@
-playerName = raw_input("Welcome to our Python RPG! To get this adventure 
-started, please tell me your name.")
+name = raw_input("Welcome to our Medieval Python RPG. To get this adventure started, please tell me your name. ")
+print "So your name is %s." % name
 
-print "So your name is %s." %playerName
+gender = raw_input("Are you a male or female?") 
 
-gender = raw_input("Are you a male or female?")
-if gender = female:
-	print "Oh, you're a girl!"
-if gender = male:
-	print "Oh, a boy, I see."
+if 'Female' in gender:
+    print "Oh, you're a girl!"
+    
+if 'Male' in gender:
+	print "Oh, a boy I see. So you are a palace scribe who has been working for the King of Karflooglesville for a 		number of years now. As a young boy, your parents sent you to work for the palace in order to pay for their overdue taxes. The King has grown fond of you." 
+	
 
-philosophy = raw_input("What would of the following would you say is your
-life philosophy? Be kind to all, do the most with the least effort,
-or money is everything?")
-
-if philosophy = Be kind to all:
-	print "Well aren't you sweet!"
-if philosophy = Do the most with the least effort:
-	print "Don't be too lazy now!"
-if philosophy = Money is everything:
-	print "If you say so... but aren't there more important things in 
-	life?"
 
