535837e7eacdec79aeef97677b4a127f4e11a4ec
odorsey <odorsey@live.unc.edu>
2013-11-20 08:45:41 -0500

    Create scribe_level1.py

---
+++ scribe_level1.py
@@ -0,0 +1,8 @@
+character1 = {'Name': 'Palace Scribe', 'Gender': 'Male', 'Role': 'Documenting the life of the king'}; 
+
+scribe_level1 = ['dungeon', 'door one', 'door two'];
+
+print "Chapter 1: Breakin' Bad Habits" 
+
+print "As a result of your actions, you’re sent to the dungeon to think about what you've done. What will you do in this dank and lonely cell?"
+
