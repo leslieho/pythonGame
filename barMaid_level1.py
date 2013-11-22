54a27b6f1edf325a72fa5e6c663ead3981216b60
odorsey <odorsey@live.unc.edu>
2013-11-22 14:20:40 -0500

    Updated after running tests
    Everything should run successfully!

--- barMaid_level1.py
+++ barMaid_level1.py
@@ -9,33 +9,35 @@ print "As a result of your actions, you’re sent to the dungeon to think about 
 action = raw_input("What will you do in this dank and lonely cell? Break out or stay dank and lonely?\n")
 if 'Stay dank and lonely' in action:
   print "If you wish..."
-  import scribe_level1.py
+  import barMaid_level1.py
 
 if 'Break out' in action:
   print "Excellent!"
-  escape = raw_input("...But how will you break out? The way I see it, you have two options. Break down the cell door with   your highly reliable beer mug or break the conveniently located window right behind you?\n")
+  escape = raw_input("...But how will you break out? The way I see it, you have two options. Break down the cell door with your highly reliable beer mug or break the conveniently located window right behind you?\n")
   if 'Break down cell door' in escape:
-  print "Alas, to no avail."
-  
+  	print "Alas, to no avail."
+  		
   if 'Break window' in escape:
-  print "Somehow you've managed to break the window quietly enough not to attract any attention from the dungeon guards or your fellow dungeon mates. You escape successfully!" 
+  	print "Somehow you've managed to break the window quietly enough not to attract any attention from the dungeon guards or your fellow dungeon mates. You escape successfully!\n" 
   
-  location = raw_input("Now that you've dusted off shards of glass while remaining unharmed, where will you go? To the      nearing forest, or back to the palace?"
-  if 'Forest' in location: 
-  import barMaid_level2.py
+location = raw_input("Now that you've dusted off shards of glass while remaining unharmed, where will you go? To the nearby forest, or back to the palace?\n")
+if 'Forest' in location: 
+  	print "Onward to the next level!" 
+  	import barMaid_level2.py
+if 'Palace' in location:
+  	print "What are you doing?! You can't go back there, the King'll have your head for sure!"
+  	decision = raw_input("Do you really want to go back?\n")
+	if 'Yes' in decision:
+  		print "The King spots you sneaking across the grounds while he is on one of his evening strolls. He is merciful and sends you back to the dungeon."
+  		import barMaid_level1.py
+  	if 'No' in decision:
+  		print "Good. I knew you were wiser than that."
+    	location = raw_input("Now that you've dusted off shards of glass while remaining unharmed, where will you go? To the nearing forest, or back to the palace?\n")
+  	if 'Forest' in location: 
+  			import barMaid_level2.py
+ 
+    
   
-  if 'Palace' in location:
-  print "What are you doing?! You can't go back there, the King'll have your head for sure!"
-  
-  decision = raw_input("Do you really want to go back?")
-  if 'Yes' in decision:
-  print "The King spots you sneaking across the grounds while he is on one of his evening strolls. He is merciful and     sends you back to the dungeon."
-  
-  if 'No' in decision:
-  print "Good. I knew you were wiser than that."
-  location = raw_input("Now that you've dusted off shards of glass while remaining unharmed, where will you go? To the      nearing forest, or back to the palace?"
-  if 'Forest' in location: 
-  import barMaid_level2.py
   
   
   
