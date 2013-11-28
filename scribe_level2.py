b4b5c93aeb0ee3ea8ff763170b86a299cd121f52
leslieho <leslieho@users.noreply.github.com>
2013-11-28 14:41:34 -0500

    Update and rename scribe level 1

---
+++ scribe_level2.py
@@ -0,0 +1,14 @@
+''' Python
+print "Welcome to Chapter 2: The Forest of Love!" 
+print " "
+print "You have successfully escaped from your cell! But you are being chased by the palace guards. You run for your life and end up crashing into a lovely maiden."
+print " "
+print "You give her a cut across her knee.  She is tearing up from the pain.  Will you " + "\"heal her cut magical quill\"" + ' or '+ "\"give her an angry look?\""
+pain_response = raw_input()
+if 'heal her cut with your magical quill' in pain_response:
+	print " "
+	print "She says thank you and asks why you, the palace scribe, is running through the forest. You reply and say that you   are running from the palace guards because you defied the king.  She then provides refuge for you in return for you healing her!"
+else:
+	print " "
+	print "She says " +"\"HMPH\""+" and runs away crying." 
+'''	
