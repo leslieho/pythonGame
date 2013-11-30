3508ff368cd0021390f01d052921095647b1cfa9
leslieho <leslieho@users.noreply.github.com>
2013-11-30 12:06:36 -0500

    Update scribe level 2

--- scribe_level2.py
+++ scribe_level2.py
@@ -18,14 +18,48 @@ if 'heal her cut with your magical quill' in pain_response:
 			print "The blacksmith laughs, creates a suit of armor for you, and you can barely walk.  The 					blacksmith laughs as you fall to the ground.  He bids you good luck and you slowly leave the 			blacksmith.  You travel to the cave where the beast is located.  Do you" + "\"enter the cave bravely\"" 			+" or "+ "\"hesitate\"" +"?"
 			cave_response = raw_input()
 			if 'enter  the cave bravely' in cave_response:
-				print "You walk into the dark cave.  You hear growling and all of a sudden, a fireball rushes your way.  The fireball hits your armor and you get flung backwards.  You get up as fast as you can in the heavy suit of armor.  You ball up your armored fist.  Do you "+'\'hit left\''+' or '+'\'hit right\''?
+				print "You walk into the dark cave.  You hear growling and all of a sudden, a fireball rushes your way.  The fireball hits your armor and you get flung backwards.  You get up as fast as you can in the heavy suit of armor.  You ball up your armored fist.  Do you "+'\'swing left\''+' or '+'\'swing right\''?
 				swing_response = raw_input()
 				if 'swing left' in swing_response:
 					print 'Yikes, you missed the beast.  Looks like you have to swing again.'
+					swing2_response=raw_input()
+					if 'swing left' in swing2_response:
+						print 'Congratulations, you have killed the beast! Your savior comes and gives you a kiss for saving her village.  She says she is forever in your debt. You two fall in love, and get happily married!  You are deemed a hero in the village's eyes and everyone is happy for your marriage!  Would you like to continue to chapter 3,' + '\' yes\'' + ' or ' + '\'no\''+'?'
+						chapter3_response=raw_input()
+						if 'yes' in chapter3_response:
+							import scribe_level3
+						if 'no' in chapter3_response:
+							print 'You have ended the game! Congratulations hero!'
+					if 'swing right' in swing2_response:
+						print 'Congratulations, you have killed the beast! Your savior comes and gives you a kiss for saving her village.  She says she is forever in your debt. You two fall in love, and get happily married!  You are deemed a hero in the village's eyes and everyone is happy for your marriage!  Would you like to continue to chapter 3,' + '\' yes\'' + ' or ' + '\'no\''+'?'
+						chapter3_response=raw_input()
+						if 'yes' in chapter3_response:
+							import scribe_level3
+						if 'no' in chapter3_response:
+							print 'You have ended the game! Congratulations hero!'
 		if 'simple sword' in blacksmith_response:
 			print 'The blacksmith smiles, creates a sword of magestic proportions, and gives it to you.  He 				calls you brave and bids you good luck.  You leave the blacksmith and travel to the cave 				where the beast is located. Do you" + "\"enter the cave\"" +" or "+ "\"hesitate\'' 			+"?"
 			cave1_response = raw_input()
 			if 'enter the cave' in cave1_response:
+				print 'You walk into the dark cave.  You hear growling and all of a sudden, a fireball rushes your way.   You quickly dodge out of the way.  In your action of dodging, you lost your sword!  You decide to use your magical quill and you quickly pull it out.  It is too dark to see where the beast is.  Will you wave your quill'+'\' left\'' +' or '+'\'right\''+'?'
+				wave_response=raw_input()
+				if 'right' in wave_response:
+					print 'print 'Yikes, you missed the beast.  Looks like you have to wave again.'
+					wave2_response=raw_input()
+					if 'right' in wave2_response:
+						'Congratulations, you have killed the beast! Your savior comes and gives you a kiss for saving her village.  She says she is forever in your debt. You two fall in love, and get happily married!  You are deemed a hero in the village's eyes and everyone is happy for your marriage!  Would you like to continue to chapter 3,' + '\' yes\'' + ' or ' + '\'no\''+'?'
+						chapter3_response=raw_input()
+						if 'yes' in chapter3_response:
+							import scribe_level3
+						if 'no' in chapter3_response:
+							print 'You have ended the game! Congratulations hero!'
+					if 'left' in wave_response:
+						print 'Congratulations, you have killed the beast! Your savior comes and gives you a kiss for saving her village.  She says she is forever in your debt. You two fall in love, and get happily married!  You are deemed a hero in the village's eyes and everyone is happy for your marriage!  Would you like to continue to chapter 3,' + '\' yes\'' + ' or ' + '\'no\''+'?'
+						chapter3_response=raw_input()
+						if 'yes' in chapter3_response:
+							import scribe_level3
+						if 'no' in chapter3_response:
+							print 'You have ended the game! Congratulations hero!'
 				
 else:
 	print " "
