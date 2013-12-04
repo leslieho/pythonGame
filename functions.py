bb1c22a3396b3563e7a4b1fd9a04b5796c685a22
leslieho <leslieho@users.noreply.github.com>
2013-12-04 16:07:49 -0500

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,6 @@
+def player_input(prompt, choice_1, choice_2):
+	"this prompts the player with the choices for the game"
+	print "Prompt: ", prompt;
+	print "Choice 1: ", choice_1;
+	print "Choice 2: ", choice_2;
+	
