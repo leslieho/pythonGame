c52a4d974b2905f9bbcbafd9386c54b450150a31
leslieho <leslieho@users.noreply.github.com>
2013-12-04 16:23:39 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
-def player_input(prompt, choice_1, choice_2):
+def player_input(prompt, a, b):
 	"this prompts the player with the choices for the game"
 	print "Prompt: ", prompt;
-	print "Choice 1: ", choice_1;
-	print "Choice 2: ", choice_2;
+	print "Choice 1: ", a;
+	print "Choice 2: ", b;
 	
