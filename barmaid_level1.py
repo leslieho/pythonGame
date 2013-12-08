03efc2e4aadc28651deb2ff36c5928bbd333c540
odorsey <odorsey@live.unc.edu>
2013-12-08 12:06:16 -0500

    Update barmaid_level1.py

--- barmaid_level1.py
+++ barmaid_level1.py
@@ -6,8 +6,7 @@ print "Chapter 1: Breakin' Bad Habits"
 print "As a result of your actions, you’re sent to the dungeon to think about what you've done.\n"
 
 while character2['Level'] == 'barmaid_level1':
-	
-	while character2['Location'] == 'Dungeon':
+while character2['Location'] == 'Dungeon':
 		action = raw_input("What will you do in this dank and lonely cell? Break out or stay dank and lonely?\n")
 		if 'Stay dank and lonely' in action:
   			print "If you wish..."
