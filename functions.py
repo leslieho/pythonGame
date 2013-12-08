fdf8d92dbe96abded81a3cb14732ac6168250a50
leslieho <leslieho@users.noreply.github.com>
2013-12-08 17:25:49 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,116 +1,14 @@
-# INTRODUCTION LEVEL 
-dict player_input(prompt, choices_list);
-player[choice] = player_input("Are you going to be a male or a female in this game?", ["Female", "Male"]) 
-input =""
+def player_input(prompt, choice_1, choice_2):
+	"this prompts the player with the choices for the game"
+	print "Prompt: ", prompt;
+	print "Choice 1: ", choice_1;
+	print "Choice 2: ", choice_2;
 
-print prompt 
-print "Your Choices Are:"
-
-for choice in choices_list:
-    print choice
-
-while input not in choices_list:
-    input = raw_input("What is your choice? \n")
-    print input
-
-# INTRODUCTION LEVEL 
-player_input(prompt, choices_list);
-player[choice] = player_input("Do you speak up or comply?", ["Speak up", "Comply"]) 
-input =""
-
-print prompt 
-print "Your Choices Are:"
-
-for choice in choices_list:
-    print choice
-
-while input not in choices_list:
-    input = raw_input("What is your choice? \n")
-    print input
-
-# SCRIBE LEVEL 1 
-player_input(prompt, choices_list);
-player[choice] = player_input("What will you do in this dank and lonely cell?", ["Break out", "Stay dank and lonely"])
-input =""
-
-print prompt 
-print "Your Choices Are:"
-
-for choice in choices_list:
-    print choice
-
-while input not in choices_list:
-    input = raw_input("What is your choice? \n")
-    print input
-
-player_input(prompt, choices_list);
-player[choice] = player_input("Do you turn your quill to the right or the left first?", ["Right", "Left"]) 
-input =""
-
-print prompt 
-print "Your Choices Are:"
-
-for choice in choices_list:
-    print choice
-
-while input not in choices_list:
-    input = raw_input("What is your choice? \n")
-    print input
-
-player_input(prompt, choices_list);
-player[choice] = player_input("Which door do you choose to leave from? Door one or door two?", ["Door one", "Door two"]) 
-input =""
-
-print prompt 
-print "Your Choices Are:"
-
-for choice in choices_list:
-    print choice
-
-while input not in choices_list:
-    input = raw_input("What is your choice? \n")
-    print input
-
-#BAR MAID LEVEL 1 
-player_input(prompt, choices_list);
-player[choice] = player_input("What will you do in this dank and lonely cell?", ["Break out", "Stay dank and lonely"]) 
-input =""
-
-print prompt 
-print "Your Choices Are:"
-
-for choice in choices_list:
-    print choice
-
-while input not in choices_list:
-    input = raw_input("What is your choice? \n")
-    print input
-
-player_input(prompt, choices_list);
-player[choice] = player_input("But how will you break out?", ["Break down door", "Break window"])
-input =""
-
-print prompt 
-print "Your Choices Are:"
-
-for choice in choices_list:
-    print choice
-
-while input not in choices_list:
-    input = raw_input("What is your choice? \n")
-    print input
-
-player_input(prompt, choices_list);
-player[choice] = player_input("Where will you go?", ["Forest", "Palace"]) #Input asking the user if he/she wants to break out of the cell (and thus play the game) or not 
-input =""
-
-print prompt 
-print "Your Choices Are:"
-
-for choice in choices_list:
-    print choice
-
-while input not in choices_list:
-    input = raw_input("What is your choice? \n")
-    print input
-
+def program_answer_response(choice_dict_name, user_input_name):
+	"prompts users incorrect answer when user inputs wrong thing"
+	while (user_input_name).lower() not in choice_dict_name:
+		print " "
+		print "Incorrect input.  Try again."
+		user_input_name=raw_input("What is your choice?")
+	if (user_input_name).lower() in choice_dict_name:
+		print "awesome"
